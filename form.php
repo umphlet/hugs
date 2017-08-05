c7b1bbe9208cd511c5405f208e80fbe9b3a07c23
umphlet <sean.umphlet@gmail.com>
2017-08-05 18:51:46 -0400

    Random style changes

--- form.php
+++ form.php
@@ -1,14 +1,9 @@
 <?
 if ($CLEAN_URL === true) {
-  if (preg_match('/hugme/i',$_SERVER["REQUEST_URI"])) {
-      $url_start = '../';
-  } else {
-      $url_start = './';
-      }
 ?>
 <form onsubmit="location.href='<? echo $url_start ?>hugme/' + document.getElementById('myInput').value; return false;">
 <? } else { ?>
-<form onsubmit="location.href='./index.php?hug=' + document.getElementById('myInput').value; return false;">
+<form onsubmit="location.href='index.php?hug=' + document.getElementById('myInput').value; return false;">
 <? } ?>
 <input type="text" id="myInput">
   <input type="submit">
--- newhug.php
+++ newhug.php
@@ -21,6 +21,6 @@
 ?>
 <h1>You hugged <? echo htmlspecialchars($name); ?></h1>
 <p><? echo htmlspecialchars($name); ?> has <? echo $addedhug ?> hugs now!</p>
-<p>Here is your HugMe URL: <a href="<? echo $HUG_URL; ?>"><? echo "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?></a></p>
+<p>Here is your HugMe URL: <a href="<? echo "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>"><? echo "http://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?></a></p>
 <p>Hug someone else?</p>
 <? include("form.php"); ?>
