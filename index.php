bec92594c9aab04d729777da960ca2c561ccb680
umphlet <sean.umphlet@gmail.com>
2017-08-06 09:46:00 -0400

    Fix BASE_URI if it is missing proper slashes

--- index.php
+++ index.php
@@ -4,21 +4,13 @@
 // Include config file
 require_once("hugs-config.php");
 
-// Connect to our database
-$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
-if (!$conn) {
-  die('Cound not connect: ' . mysqli_connect_error());
-}
-
-// Check if CLEAN_URL is enabled and set page links accordingly
-if ($CLEAN_URL == true) {
-  $HUG_URL = "./" . $_GET["hug"];
-} else {
-  $HUG_URL = "./index.php?hug=" . $_GET["hug"];
-}
+// Check and clean up settings
+require_once("settings-check.php");
 
 // Main content file
 require_once("content.php");
+
+// Footer
 require_once("footer.php");
 ?>
 </body>
---
+++ settings-check.php
@@ -0,0 +1,22 @@
+<?
+// Connect to our database
+$conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
+if (!$conn) {
+  die('Cound not connect: ' . mysqli_connect_error());
+}
+
+// Check if CLEAN_URL is enabled and set page links accordingly
+if ($CLEAN_URL == true) {
+  $HUG_URL = "/" . $_GET["hug"];
+} else {
+  $HUG_URL = "/index.php?hug=" . $_GET["hug"];
+}
+
+// Check if BASE_URI has proper slashes...
+if (preg_match("/^\//i", $BASE_URI) == 0) {  
+  $BASE_URI = "/" . $BASE_URI;
+}
+if (preg_match("/\/$/i", $BASE_URI) == 0) { 
+  $BASE_URI .= "/";
+}
+?>
